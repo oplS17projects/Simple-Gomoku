5650c4a21639b5a3a2f6d5ff0d55eb2e01dc7097
Xiaoling Zheng <xlzhen@users.noreply.github.com>
2017-04-09 16:09:40 -0400

    Update main.rkt

--- code/main.rkt
+++ code/main.rkt
@@ -3,7 +3,7 @@
 (require "make-game.rkt")
 (require "calc-best-place-black-stone.rkt")
 
-;; a game
+;; game
 
 (define pve 'ai) ;; we assume ai has black stones; it will always be player-one
 (define pvp 'pvp) ;; two persons start from placing black stones; player-one, player-two, player-one, ...
@@ -13,20 +13,21 @@
        (game (make-game))
        (result #f)) ;; represents no one wins will be updated after each set-piece
     
-    (if (equal? pve 'ai)
-        (pve #f)
-        (pvp #f))
+    (define (start)
+      (if (equal? mode 'ai)
+          (pve #f)
+          (pvp #f)))
     
     (define (get-coord) (calc-black-stone (game 'get-black-list) (game 'get-white-list)))
     ;; call procedure calculate best coord to place black stone
     
     (define (pve status)
-      (if (status)
+      (if (equal? status #t)
           (win-prompt)
           (let ((coord (get-coord (game 'get-black-list) (game 'get-white-list))))
             (cond[(equal? (game 'set-black-piece (car coord) (cdr coord)) 'black) (set! result 'black)
                                                                                   (pve #t)]
-                 [else '()]
+                 [else #f]
                  ;;  [else (get white from input - coord)
                  ;;        (cond[(equal? (game 'set-white-piece (car coord) (cdr coord) 'white)(set! result 'white)
                  ;;                                                                             (pve #t)]
@@ -34,9 +35,9 @@
                ))))
 
     (define (pvp status)
-      (if (status)
+      (if (equal? status #t)
           (win-prompt)
-          (#f)
+          #f))
           ;; -- (get black piece from input)
           ;; -- (cond [(set black piece) == 'black) (set! result 'black)
           ;;                                        (pvp #t)]
@@ -44,17 +45,10 @@
           ;;                (cond[(set white piece) == 'white) (set! result white)
           ;;                                                   (pvp #t)]
           ;;                     [else (pvp #f)])])
-          ))
+          
     
      (define (win-prompt)
        (display result))
-            
-     ;; if (equal? pve 'ai)
-       ;; black - get-input (calc-black-stone (game 'get-black-list) (game 'get-white-list))- set-piece 
-       ;; white - get-input - set-piece 
-    ;; else 
-       ;; calc-piece - set-piece 
-       ;; white - get-input - set-piece
-    )
+    start))
 
  
