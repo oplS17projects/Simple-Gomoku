6088903a85ede98821318c25d782acfefe39a8d1
Ruowei <ruozhang929@gmail.com>
2017-04-16 21:23:14 -0400

    Delete calc-best-place-black-stone.rkt

--- code/calc-best-place-black-stone.rkt
+++
@@ -1,7 +0,0 @@
-#lang racket
-
-(provide calc-black-stone)
-
-
-(define (calc-black-stone placed-black-stones placed-white-stones)
-  (list 1 1)) ;; testing procedure which will always place black-piece to 1 1
