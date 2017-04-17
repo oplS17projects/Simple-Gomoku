975194aaaa953b159fad0235562616c1e9069e72
Xiaoling Zheng <xlzhen@users.noreply.github.com>
2017-04-16 21:24:50 -0400

    Add files via upload

---
+++ code/calc-best-place-black-stone.rkt
@@ -0,0 +1,24 @@
+#lang racket
+
+(provide calc-black-stone)
+
+
+(define (calc-black-stone placed-black-stones placed-white-stones)
+  (list 1 1)) ;; testing procedure which will always place black-piece to 1 1
+
+;; list all the threats
+;; creating fork
+;; 2 - fours
+;; 1 four and one opened three
+;; list all the threats
+;;
+;; evaluate each piece based on.... 
+;; must wins: 
+;; store opened 3-stone 's
+;; store opened/ one-ended 4-stones
+
+;; block opponent's 4 stones, opened 3 stones
+;;
+;; find forking opportunities
+;; 
+
