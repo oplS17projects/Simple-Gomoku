9b1ad580fcd40fe6034dd53ed313dfc669cdbf4b
Ruowei <ruozhang929@gmail.com>
2017-04-29 22:47:27 -0400

    Update calc-stone.rkt

--- code/calc-stone.rkt
+++ code/calc-stone.rkt
@@ -1,13 +1,14 @@
 #lang racket
 
-;; works as a stub function ;;
+;;This function will return a coordinate and the coordinate will be used in main.rkt. 
+;;The program then will draw black stones in PVE mode.
 
 (provide calc-stone)
 
 ;(define list1 '((1 1) (1 2) (3 4) (5 6) (3 5))) ;; test list
 ;(define list2 '((3 1) (10 2) (3 14) (9 6) (0 5))) ;; test list
 
-;;http://stackoverflow.com/questions/23133351/position-of-minimum-element-in-list
+;;Credit: http://stackoverflow.com/questions/23133351/position-of-minimum-element-in-list
 (define (min-position xs)
     (define (min-position2 count pos xs)
       (cond ((null? xs) #f)
@@ -17,46 +18,25 @@
             (else (min-position2 0 (+ count pos 1) (cons (cadr xs) (cddr xs))))))
     (min-position2 0 0 xs))
 
+;;this function takes 3 parameters(black occupied list, white occupied list and the board list) and return a selected coordinate.
+
 (define (calc-stone placed-black-stones placed-white-stones board-coord)
   
-  (define r (list 'nil))
-  (set! r (append placed-black-stones placed-white-stones))
-  (define f (remove* r board-coord))
-  (define c '(7 7))
-  (define z (remove* c f))
-  ;(define select-random
-    ;(lambda (z)
-      ;(let ((len (length z)))         
-        ;(list-ref z (random len)))))
+  (define occupiedlst (list 'nil))
+  (set! occupiedlst (append placed-black-stones placed-white-stones))
+  (define emptylst (remove* occupiedlst board-coord))
+  (define center '(7 7))
+  (define emptylst2 (remove* center emptylst))
+  
   (define x (map (lambda (y)
                    (floor (sqrt
                     (+ (* (- (car y) 7) (- (car y) 7))
                      (* (- (cadr y) 7) (- (cadr y) 7))))))
-                 z))
-  (list-ref z (min-position x))
-  ;(select-random z)
-  ;'(10 10)
-
-
-  
-  ;(sqrt (+ (* (- (car s) 7) (- (car s) 7)) (* (- (cadr s) 7) (- (car s) 7)))))
+                 emptylst2))
+  (list-ref emptylst2 (min-position x))
 )
-;(define s (select-random z))
-;(list (car s) (cadr s))
-;'(10 10)
-  
-   
-;(define (check a b)
-; (if (and (not (member (list a b) placed-black-stones))
-;         (not (member (list a b) placed-white-stones)))
-;   (list a b)
-;  (if (= a 14)
-;     (check 0 (+ b 1))
-;    (check (+ a 1) b))))
   
 
-
-;; testing procedure which will always place black-piece to 1 1
 
 ;; list all the threats
 ;; creating fork
